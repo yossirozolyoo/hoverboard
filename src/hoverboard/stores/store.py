d7c3efcb1fd70aeb2b08eaa4267b1b3cd31a5458
Yossi Rosilio <yossirozolyoo@hotmail.com>
2023-04-10 18:07:43 +0300

    Added store registration feature

--- src/hoverboard/stores/store.py
+++ src/hoverboard/stores/store.py
@@ -4,15 +4,20 @@ import uuid
 from typing import IO
 
 
+_stores = {}
+
+
 class BinaryStore:
     """
     Represents a binary store on the disk.
     """
-    def __init__(self, path: str = None):
+    def __init__(self, path: str = None, name: str = None):
         """
         Initializes the `BinaryStore` instance.
 
         :param path: The path the store is in. `None` for a temporary path.
+        :param name: The name to give to the store. `None` for no registration
+            of the store.
         """
         if path is None:
             path = os.path.join(tempfile.gettempdir(), str(uuid.uuid4()))
@@ -22,6 +27,13 @@ class BinaryStore:
                 raise FileNotFoundError(path)
 
         self._path = path
+        self._name = name
+
+        if name is not None:
+            if name in _stores:
+                raise KeyError(f'The name {repr(name)} is already taken')
+
+            _stores[name] = self
 
     @property
     def path(self) -> str:
@@ -50,8 +62,28 @@ class BinaryStore:
         :param name: The inner store name
         :return: The created store
         """
+        if self._name is not None:
+            store_name = f'{self._name}:{name}'
+            if store_name in _stores:
+                return _stores[store_name]
+        else:
+            store_name = None
+
         store_path = os.path.join(self._path, name)
         if not os.path.isdir(store_path):
             os.mkdir(store_path)
 
-        return BinaryStore(store_path)
+        if self._name is None:
+            return BinaryStore(store_path)
+        else:
+            return BinaryStore(store_path, store_name)
+
+    @staticmethod
+    def get(name: str) ->'BinaryStore':
+        """
+        Get a previously opened store.
+
+        :param name: The name of the opened store.
+        :return: The store.
+        """
+        return _stores[name]
