93bc9626491c6516ed3109b99955cc2c9f639226
Yossi Rosilio <yossirozolyoo@hotmail.com>
2023-04-16 23:25:16 +0300

    Changed toolchain's api

--- src/hoverboard/toolchains/toolchain.py
+++ src/hoverboard/toolchains/toolchain.py
@@ -1,27 +1,72 @@
-from abc import abstractmethod
+import os
 from collections.abc import Mapping
 from ..types import HierarchyMapping
 from ..tools import Tool
+from ..stores import BinaryStore
 from typing import Union, Iterable, Tuple, Iterator, Mapping as MappingT
 
 Metadata = MappingT[str, str]
 Other = Union[MappingT[str, Tool], Iterable[Tuple[str, Tool]]]
 
 
+DEFAULT_METADATA = {
+    'name': None,
+    'version': '0.0.0.0',
+    'tools': {}
+}
+
+
 class Toolchain(MappingT, Mapping):
     """
     Represents a toolchain.
     """
-    def __init__(self, tools: Other, metadata: Metadata = None):
+    __metadata__ = {}
+
+    def __init__(self, metadata: Metadata = None, path: Union[str, BinaryStore] = None):
         """
         Initializes the `Toolchain` instance.
 
-        :param tools: The tools in the toolchain
-        :param metadata: The metadata of the toolchain
+        :param metadata: The metadata of the toolchain, overrides __metadata__
         """
         super().__init__()
-        self._tools = HierarchyMapping(tools)
-        self._metadata = HierarchyMapping(metadata)
+
+        # Init metadata
+        self._metadata = HierarchyMapping(self.__metadata__)
+        if metadata is not None:
+            self._metadata.update(metadata)
+
+        if self._metadata.name is None:
+            raise ValueError('Missing section "name" in metadata')
+
+        # Init tools
+        if path is None:
+            if 'path' in self._metadata:
+                path = self._metadata['path']
+            else:
+                path = None
+
+        if isinstance(path, str):
+            path = BinaryStore(name=self._metadata.name, path=path)
+
+        # Init tools
+        self._tools = HierarchyMapping()
+        for tool_name, tool_metadata in self._metadata.tools.top_level.items():
+            tool_metadata = tool_metadata.copy()
+            if 'path' not in tool_metadata:
+                raise ValueError(f"'path' not in {repr(tool_name)} metadata")
+
+            if path is not None:
+                tool_metadata['path'] = path.get_path(tool_metadata['path'])
+
+            if 'name' not in tool_metadata:
+                tool_metadata['name'] = tool_name
+
+            if 'base-class' in tool_metadata:
+                base_class = tool_metadata['base-class']
+            else:
+                base_class = Tool
+
+            self._tools[tool_name] = base_class.create(tool_metadata)
 
     def __iter__(self) -> Iterator[str]:
         """
