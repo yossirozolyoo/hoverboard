de58f14f41ecd54c3b097b6d3aeb5872fdeb7e63
Yossi Rosilio <yossirozolyoo@hotmail.com>
2023-04-20 20:53:44 +0300

    Implemented auto registration of toolchains

--- src/hoverboard/toolchains/store.py
+++ src/hoverboard/toolchains/store.py
@@ -2,12 +2,47 @@ from . import toolchain as t
 from .installation_database import DefaultInstallationDatabase
 from ..tools import register as register_tool
 from ..types import HierarchyMapping
-from typing import Callable
+from typing import Callable, Type
 
 LoadToolchain = Callable[[HierarchyMapping], 't.Toolchain']
+ToolchainImplementation = Type['t.Toolchain']
 
 toolchains = {}
 dbs = [DefaultInstallationDatabase]
+types = {}
+
+
+def register_implementation(name: str, implementation: ToolchainImplementation):
+    """
+    Register an implementation under a given name.
+
+    :param name: The name of the implementation
+    :param implementation: The implementation
+    """
+    if name in types:
+        raise KeyError(f'Implementation {repr(name)} already exists')
+
+    types[name] = implementation
+
+
+def implementation(name: str) -> Callable[[ToolchainImplementation], ToolchainImplementation]:
+    """
+    Returns a decorator that registers an implementation with a given name.
+
+    :param name: The name of the implementation
+    :return: A decorator that should wrap the implementation
+    """
+    def hook(cls: ToolchainImplementation) -> ToolchainImplementation:
+        f"""
+        Wraps an implementation of a toolchain to register it under the name {repr(name)}.
+
+        :param cls: The implementation of the toolchain
+        :return: The same implementation of the toolchain
+        """
+        register_implementation(name, cls)
+        return cls
+
+    return hook
 
 
 def register(toolchain: 't.Toolchain', register_tools: bool = True):
@@ -34,25 +69,39 @@ def install(path: str, metadata: HierarchyMapping):
     DefaultInstallationDatabase.install(path, metadata)
 
 
-def get(name: str, load: LoadToolchain = None, register_tools: bool = True) -> 't.Toolchain':
+def get(name: str) -> 't.Toolchain':
     """
     Get a registered toolchain. Raises `KeyError` if the toolchain wasn't registered before.
 
-    If `load` is supplied, then the toolchain is searched in known `InstallationDatabase`s.
-
     :param name: The name of the toolchain
-    :param load: A function that receives the toolchain's metadata and returns the toolchain object from it.
-    :param register_tools: If a toolchain is loaded from disk, whether to register its tools
     :return: The found Toolchain instance.
     """
     if name in toolchains:
         return toolchains[name]
 
-    if load is not None:
-        for installation_database in dbs:
-            if name in installation_database.installed:
-                toolchain = installation_database.installed[name]
-                register(toolchain, register_tools)
-                return toolchain
+    for installation_database in dbs:
+        if name in installation_database.installed:
+            metadata = installation_database.installed[name]
+
+            if 'register-tools' in metadata:
+                register_tools = metadata['register-tools']
+            else:
+                register_tools = True
+
+            if 'type' in metadata:
+                toolchain_type_name = metadata['type']
+                if toolchain_type_name not in types:
+                    raise KeyError(f'Unknown toolchain type {repr(toolchain_type_name)}')
+
+                toolchain_type = types[toolchain_type_name]
+            elif metadata['name'] in types:
+                toolchain_type = types[metadata['name']]
+            else:
+                toolchain_type = t.Toolchain
+
+            toolchain = toolchain_type(metadata=metadata)
+            register(toolchain, register_tools)
+
+            return toolchain
 
     raise KeyError(f"Couldn't find a registered toolchain named {repr(name)}")
